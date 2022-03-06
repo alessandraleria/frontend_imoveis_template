acaf420a554a43551fa40ad5bd0b5fcb0d7b13b6
dvalessandraleria <alessandra@orbitstechnology.com>
2022-03-06 12:41:07 -0300

    update: começa funcionamento do maps

--- src/views/pages/dashboard/index.js
+++ src/views/pages/dashboard/index.js
@@ -3,31 +3,46 @@ import { useEffect, useState } from 'react';
 import { Grid } from '@mui/material';
 // project imports
 import { gridSpacing } from 'store/constant';
-import { GoogleMap, Marker, withGoogleMap, withScriptjs } from 'react-google-maps';
+// import { GoogleMap, Marker, withGoogleMap, withScriptjs } from 'react-google-maps';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 // ==============================|| SAMPLE PAGE ||============================== //
-const MyMapComponent = withScriptjs(withGoogleMap((props) =>
-  <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
-  >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
-  </GoogleMap>
-))
-export default function SamplePage() {
+
+function SamplePage({google, locations = []}) {
     return (
         <Grid container spacing={gridSpacing}>
             <Grid item xs={12}>
-                <MyMapComponent
+                {/* <MyMapComponent
                     isMarkerShown
                     googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
                     loadingElement={<div style={{ height: `100%` }} />}
                     containerElement={<div style={{ height: `700px` }} />}
                     mapElement={<div style={{ height: `100%` }} />}
-                />
+                /> */}
+                <Map
+                    google={google}
+                    containerStyle={{
+                        position: "fixed",
+                        width: "83%",
+                        height: "80%"
+                    }}
+                    style={{
+                        width: "100%",
+                        height: "100%"
+                    }}
+                    center={locations[0]}
+                    zoom={locations.length === 1 ? 18 : 13}
+                    disableDefaultUI={true}
+                    initialCenter={{lat: -23.528024418017562, lng: -47.49669579836341}}
+                > 
+                    <Marker position={{lat: -23.528024418017562, lng: -47.49669579836341}}/>    
+                </Map>
             </Grid>
         </Grid>
     );
 }
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyBHH-GUvxY2zTD3xhocuuW8UZZAID3TEyM'
+})(SamplePage);
 
 /*
 export class MapContainer extends Component {
